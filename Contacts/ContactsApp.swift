7f4a608fafcb26d3bdf7db5cd910f7084215726a
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-04-08 21:02:20 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Лидия Ладанюк on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
